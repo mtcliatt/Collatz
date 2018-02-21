55ff245778209fe28b2c2359cb316d4a3cbda553
Matt Cliatt <mtcliatt@google.com>
2018-02-21 00:47:45 -0800

    Add flag for optionally skipping evens; style changes

--- graphics/binary_paths/app.js
+++ graphics/binary_paths/app.js
@@ -9,10 +9,13 @@ const colors = [
 /** Flags object contains all app parameters, aside from the colors. */
 const flags = {};
 
+/** Collatz */
 // Max number and max iterations for computing the Collatz array.
 flags.nMax = 100;
 flags.kMax = 160;
 
+
+/** Spacing */
 // Vertical spacing between each N.
 flags.nSpacing = 4;
 
@@ -22,59 +25,69 @@ flags.kSpacing = 8;
 // Vertical change between each pair of successive vertices on a line.
 flags.yDelta = 8;
 
+
+/** Misc. preferences */
 // Set to false for randomly colored lines.
 flags.gradient = true;
+
+// Set to false to render paths for even numbers as well as odd numbers.
+flags.skipEvens  = false;
 
 // Set to false to repeat Collatz for kMax iterations instead of stopping early.
 flags.stopCollatzAtOne = true;
 
-/** Initialize the canvas and render the display. */
+
+/** Run the app. */
 (() => {
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.width = flags.kMax * flags.kSpacing;;
-canvas.height = flags.nMax * flags.nSpacing;
+  /* Initialize the canvas. */
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  canvas.width = flags.kMax * flags.kSpacing;;
+  canvas.height = flags.nMax * flags.nSpacing;
 
-const collatz = [];
-computeCollatz();
-render();
+  /* Compute the Collatz array. */
+  const collatz = [];
+  computeCollatz();
 
-function computeCollatz() {
-  for (let currentN = 1; currentN < flags.nMax; currentN++) {
-    let value = currentN;
-    const nCollatz = [value];
+  /* Render the graphic. */
+  render();
 
-    for (let currentK = 1; currentK < flags.kMax; currentK++) {
-      value = value % 2 === 0 ? (value / 2) : (3 * value + 1);
-      nCollatz.push(value);
+  function computeCollatz() {
+    for (let currentN = 1; currentN < flags.nMax; currentN++) {
+      let value = currentN;
+      const nCollatz = [value];
 
-      if (flags.stopCollatzAtOne && value === 1) {
-        break;
+      for (let currentK = 1; currentK < flags.kMax; currentK++) {
+        value = value % 2 === 0 ? (value / 2) : (3 * value + 1);
+        nCollatz.push(value);
+
+        if (flags.stopCollatzAtOne && value === 1) {
+          break;
+        }
       }
-    }
 
-    collatz.push(nCollatz);
+      collatz.push(nCollatz);
+    }
   }
-}
 
-function render() {
-  for (let n = 0; n < collatz.length; n += 2) {
-    if (flags.gradient) {
-      ctx.strokeStyle = colors[n % colors.length];
-    } else {
-      ctx.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+  function render() {
+    for (let n = 0; n < collatz.length; n += flags.skipEvens ? 2 : 1) {
+      if (flags.gradient) {
+        ctx.strokeStyle = colors[n % colors.length];
+      } else {
+        ctx.strokeStyle = colors[Math.floor(Math.random() * colors.length)];
+      }
+
+      let currentY = (n + 1) * flags.nSpacing;
+      ctx.beginPath();
+      ctx.moveTo(0, currentY);
+
+      for (let k = 0; k < collatz[n].length - 1; k++) {
+        currentY += collatz[n][k] % 2 === 0 ? flags.yDelta : -flags.yDelta;
+        ctx.lineTo((k + 1) * flags.kSpacing, currentY);
+      }
+
+      ctx.stroke();
     }
-
-    let currentY = (n + 1) * flags.nSpacing;
-    ctx.beginPath();
-    ctx.moveTo(0, currentY);
-
-    for (let k = 0; k < collatz[n].length - 1; k++) {
-      currentY += collatz[n][k] % 2 === 0 ? flags.yDelta : -flags.yDelta;
-      ctx.lineTo((k + 1) * flags.kSpacing, currentY);
-    }
-
-    ctx.stroke();
   }
-}
 })();
