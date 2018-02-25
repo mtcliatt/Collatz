f2a6a58f051b84bc30c11c46ab8c5c2780e5f54e
Matthew Cliatt <matthewcliatt@gmail.com>
2018-02-24 23:19:14 -0800

    Cleans up the app with refactoring, comments, etc. (#1)

--- binary_paths/app.js
+++ binary_paths/app.js
@@ -3,17 +3,9 @@
 /* red, blue, green, yellow */
 const colors = ['#F44336', '#2196F3', '#4CAF50', '#FFEB3B'];
 
-/** Flags object contains all app parameters, aside from the colors. */
 const flags = {};
 
-
-/** App. preferences */
-// Set to false to render paths for even numbers as well as odd numbers.
-flags.skipEvens  = false;
-
-// Set to false to repeat Collatz for kMax iterations instead of stopping early.
-flags.stopCollatzAtOne = true;
-
+/* Display */
 // Set to true to see each line added one by one
 flags.animate = true;
 
@@ -24,14 +16,22 @@ flags.animationTime = 200;
 flags.drawTree = false;
 
 
-/** Collatz */
-// Max number and max iterations for computing the Collatz array.
+/* Collatz */
+// Inclusive range of values for N.
 flags.nStart = 1;
 flags.nStop = 800;
-flags.kMax = 800;
+
+// Max iterations to spend calculating the path of for any initial value of N.
+flags.maxIterations = 800;
+
+// Stop computing the path when N=1, or continue for maxIterations.
+flags.stopCollatzAtOne = true;
+
+// Set to true to only compute the path of odd values of N.
+flags.skipEvenNumbers = true;
 
 
-/** Spacing */
+/* Spacing */
 // Vertical spacing between each N.
 flags.nSpacing = 8;
 
@@ -44,34 +44,93 @@ flags.yDelta = 8;
 
 /** Run the app. */
 (() => {
-  /* Initialize the canvas. */
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  canvas.width = flags.kMax * flags.kSpacing;;
-  canvas.height = (flags.nStop - flags.nStart) * flags.nSpacing;
 
-  /* Compute the Collatz array. */
-  const collatz = [];
-  computeCollatz();
+  // Faster parity test.
+  const isEven = n => (n & 1) === 0;
 
-  /* Render the graphic. */
-  render();
+  /**
+   * Computes the Collatz array.
+   * The Collatz array is a matrix where each row's first value is N, and
+   * each consecutive value in the row is obtained by plugging the previous
+   * value in the row into this function:
+   *
+   *   f(n) = {
+   *        n / 2,   n even
+   *       3n + 1,   n odd
+   *   }
+   */
+  const computeCollatzArray = () => {
+    const collatzArray = [];
 
-  function computeCollatz() {
-    for (let n = flags.nStart; n < flags.nStop; n++) {
+    for (let n = flags.nStart; n <= flags.nStop; n++) {
+      const row = [n];
       let value = n;
-      const nCollatz = [value];
 
-      for (let currentK = 1; currentK < flags.kMax; currentK++) {
-        value = value % 2 === 0 ? (value / 2) : (3 * value + 1);
-        nCollatz.push(value);
+      for (let k = 1; k <= flags.maxIterations; k++) {
+        value = isEven(value) ? value / 2 : 3 * value + 1;
+        row.push(value);
 
         if (flags.stopCollatzAtOne && value === 1) {
           break;
         }
       }
 
-      collatz.push(nCollatz);
+      collatzArray.push(row);
+    }
+
+    return collatzArray;
+  };
+
+  /** Locates and sizes the canvas to the specified dimensions. */
+  const initCanvas = (width, height) => {
+    const canvas = document.getElementById('canvas');
+
+    canvas.width = width;
+    canvas.height = height;
+
+    return canvas;
+  };
+
+  console.log('Computing the Collatz Array.');
+  const collatzArray = computeCollatzArray();
+
+  const calculatedWidth = flags.kSpacing * flags.maxIterations;
+  const calculatedHeight = flags.nSpacing * (flags.nStop - flags.nStart);
+
+  const canvas = initCanvas(calculatedWidth, calculatedHeight);
+  const context = canvas.getContext('2d');
+
+  console.log('Rendering the graphic.');
+  render(context, collatzArray);
+
+  async function render(context, paths) {
+
+    for (let n = 0; n < paths.length; n++) {
+      // Cycle through the available colors.
+      context.strokeStyle = colors[n % colors.length];
+
+      // To draw the tree shape, start each line at the same Y.
+      let y = flags.drawTree ? 200 : flags.nSpacing * (n + 1);
+      context.beginPath();
+      context.moveTo(0, y);
+
+      for (let k = 0; k < paths[n].length - 1; k++) {
+        y += isEven(paths[n][k]) ? flags.yDelta : -flags.yDelta;
+        context.lineTo(flags.kSpacing * k, y);
+      }
+
+      if (flags.animate) {
+        const startTime = performance.now();
+        context.stroke();
+        const timeToDrawStroke = performance.now() - startTime;
+        const delay = flags.animationTime - timeToDrawStroke;
+
+        // If the delay is just a few ms, we can ignore it for performance.
+        if (delay > 5) {
+          await wait(flags.animationTime)
+        }
+      }
+      context.stroke();
     }
   }
 
@@ -81,34 +140,4 @@ flags.yDelta = 8;
     });
   }
 
-  async function render() {
-
-    for (let n = 0; n < collatz.length; n += flags.skipEvens ? 2 : 1) {
-      ctx.strokeStyle = colors[n % colors.length];
-
-      // To draw the tree shape, start each line at the same Y.
-      let currentY = flags.drawTree ? 200 : (n + 1) * flags.nSpacing;
-      ctx.beginPath();
-      ctx.moveTo(0, currentY);
-
-      for (let k = 0; k < collatz[n].length - 1; k++) {
-        currentY += collatz[n][k] % 2 === 0 ? flags.yDelta : -flags.yDelta;
-        ctx.lineTo((k + 1) * flags.kSpacing, currentY);
-      }
-
-      if (flags.animate) {
-        const startTime = performance.now();
-        ctx.stroke();
-        const timeToDrawStroke = performance.now() - startTime;
-        const delay = flags.animationTime - timeToDrawStroke;
-
-        // If the delay is just a few ms, we can ignore it for performance.
-        if (delay > 5) {
-          await wait(flags.animationTime)
-        }
-      }
-
-      ctx.stroke();
-    }
-  }
 })();
